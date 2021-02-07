262f5830ff33516f7d5fb313dc1c4a3dcf9c289d
Alexander Pawinski <apawinski@gmail.com>
2021-02-07 17:22:22 -0500

    Initial Commit

---
+++ Shared/SwiftUICombineBodyRecognitionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineBodyRecognitionDemoApp.swift
+//  Shared
+//
+//  Created by Alexander Pawinski on 2021-02-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineBodyRecognitionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
